01dd5eb2434861dd288119b6c536b1ca48fd2b33
quanglong292 <quang.long292@gmail.com>
2025-04-21 17:57:50 +0700

    feat(css-collision): fix css-in-js collision issue

--- container/src/App.js
+++ container/src/App.js
@@ -2,14 +2,24 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
+import {
+  createGenerateClassName,
+  StylesProvider,
+} from "@material-ui/core/styles";
+
+const gCls = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={gCls}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
